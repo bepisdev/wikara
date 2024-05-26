447858647281e4a679200e83cd1c8e1c9fed27fd
Josh Burns <joshyburnss@gmail.com>
2024-05-26 15:32:16 +1000

    pages now render spaces in the title

--- pkg/page/page.go
+++ pkg/page/page.go
@@ -1,31 +1,21 @@
 package page
 
 import (
-	"fmt"
-	"html/template"
 	"os"
+	"html/template"
 	"path/filepath"
-
-	"github.com/gomarkdown/markdown"
-	"github.com/gomarkdown/markdown/html"
-	"github.com/gomarkdown/markdown/parser"
-	"github.com/joshburnsxyz/wikara/pkg/utils"
-	"github.com/spf13/viper"
+	"log"
 )
 
 // Page struct holds the title and body of a wiki page.
 type Page struct {
 	Title string
+	FTitle string
 	Body  []byte
 	HtmlContent template.HTML
 }
 
 const fileExtension = ".txt"
-
-func getContentDir() string {
-	p := fmt.Sprintf("%s/%s", utils.GetExecPath(), viper.GetString("ContentDir"))
-	return p
-}
 
 // Save method writes the Page's content to a text file.
 func (p *Page) Save() error {
@@ -33,6 +23,7 @@ func (p *Page) Save() error {
 	if err := ensureDir(dataDir); err != nil {
 		return err
 	}
+
 	filename := filepath.Join(dataDir, p.Title+fileExtension)
 	return os.WriteFile(filename, p.Body, 0600)
 }
@@ -43,31 +34,12 @@ func LoadPage(title string) (*Page, error) {
 	filename := filepath.Join(dataDir, title+fileExtension)
 	body, err := os.ReadFile(filename)
 	html := template.HTML(mdToHTML(body))
+	ftitle := formatTitle(title)
 	if err != nil {
 		return nil, err
 	}
-	return &Page{Title: title, Body: body, HtmlContent: html}, nil
+	p := &Page{Title: title, FTitle: ftitle, Body: body, HtmlContent: html}
+	log.Println(p)
+	return p, nil
 }
 
-// ensureDir checks if a directory exists, and creates it if it does not.
-func ensureDir(dirName string) error {
-	err := os.MkdirAll(dirName, os.ModePerm)
-	if err != nil {
-		return fmt.Errorf("failed to create directory: %v", err)
-	}
-	return nil
-}
-
-func mdToHTML(md []byte) []byte {
-	// create markdown parser with extensions
-	extensions := parser.CommonExtensions | parser.AutoHeadingIDs | parser.NoEmptyLineBeforeBlock
-	p := parser.NewWithExtensions(extensions)
-	doc := p.Parse(md)
-
-	// create HTML renderer with extensions
-	htmlFlags := html.CommonFlags | html.HrefTargetBlank
-	opts := html.RendererOptions{Flags: htmlFlags}
-	renderer := html.NewRenderer(opts)
-
-	return markdown.Render(doc, renderer)
-}
---
+++ pkg/page/utils.go
@@ -0,0 +1,47 @@
+package page
+
+import (
+	"github.com/joshburnsxyz/wikara/pkg/utils"
+	"github.com/spf13/viper"
+	"github.com/gomarkdown/markdown"
+	"github.com/gomarkdown/markdown/html"
+	"github.com/gomarkdown/markdown/parser"
+	"fmt"
+	"os"
+	"regexp"
+	"strings"
+)
+
+func getContentDir() string {
+	p := fmt.Sprintf("%s/%s", utils.GetExecPath(), viper.GetString("ContentDir"))
+	return p
+}
+
+// ensureDir checks if a directory exists, and creates it if it does not.
+func ensureDir(dirName string) error {
+	err := os.MkdirAll(dirName, os.ModePerm)
+	if err != nil {
+		return fmt.Errorf("failed to create directory: %v", err)
+	}
+	return nil
+}
+
+func mdToHTML(md []byte) []byte {
+	// create markdown parser with extensions
+	extensions := parser.CommonExtensions | parser.AutoHeadingIDs | parser.NoEmptyLineBeforeBlock
+	p := parser.NewWithExtensions(extensions)
+	doc := p.Parse(md)
+
+	// create HTML renderer with extensions
+	htmlFlags := html.CommonFlags | html.HrefTargetBlank
+	opts := html.RendererOptions{Flags: htmlFlags}
+	renderer := html.NewRenderer(opts)
+
+	return markdown.Render(doc, renderer)
+}
+
+func formatTitle(title string) string {
+	re := regexp.MustCompile(`[A-Z][^A-Z]*`)
+	sub := re.FindAllString(title, -1)
+	return strings.Join(sub, " ")
+}
