77265e9d0a9abe983df2cb9db6fa78597a3ee426
Josh Burns <joshyburnss@gmail.com>
2024-05-26 17:08:41 +1000

    inject secure headers into handler

--- pkg/api/api.go
+++ pkg/api/api.go
@@ -10,6 +10,12 @@ var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
 // makeHandler creates a handler to validate the URL path.
 func MakeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		// Set Secure Headers for XSS protection
+		w.Header().Set("X-XSS-Protection", "1; mode=block")
+		w.Header().Set("X-Content-Type-Options", "nosniff")
+		w.Header().Set("X-Frame-Options", "deny")
+
+		// Validate URL Path
 		m := validPath.FindStringSubmatch(r.URL.Path)
 		if m == nil {
 			http.NotFound(w, r)
